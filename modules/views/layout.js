ef9df5ec493f711ef6a3bb09018e16b056369063
Alfi Salvacion <alfisalvacion@gmail.com>
2024-08-22 22:02:27 +0800

    Fix sidebar not showing by default

--- modules/views/layout.js
+++ modules/views/layout.js
@@ -238,7 +238,7 @@ define([
 
         $pages.on('pagebeforeshow', function () {
             var $page = $(this),
-                pageLayout = $page.data('layout') || 'main',
+                pageLayout = $page.data('layouted') || 'main',
                 pageSkin = $page.data('skin') || 'light';
 
             if (currentLayout != pageLayout) {
